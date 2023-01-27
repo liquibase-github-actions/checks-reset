ad59b1f491d48d6e246e4859d11b44142a9e4186
liquibot <liquibot@liquibase.org>
2023-01-27 15:14:11 +0000

    auto generated v4.19.0

--- checks_reset.sh
+++ checks_reset.sh
@@ -259,4 +259,4 @@ if [[ -n "$USEPROCEDURESCHEMA" ]]; then
 	GLOBALS+=("--use-procedure-schema=$USEPROCEDURESCHEMA")
 fi
 
-docker-entrypoint.sh "${GLOBALS[@]}" pro checks reset "${PARAMS[@]}"
+docker-entrypoint.sh "${GLOBALS[@]}" checks reset "${PARAMS[@]}"
